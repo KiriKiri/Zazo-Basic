c79c2edb1e4af44045a1a526e891be438c72d45a
Kirill Kirikov <kirillkirikov@MBP-Kirill.lan>
2015-03-19 16:11:53 +0200

    Initial Commit

---
+++ ZazoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZazoTest
+//
+//  Created by Kirill Kirikov on 19.03.15.
+//  Copyright (c) 2015 Seductive LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
